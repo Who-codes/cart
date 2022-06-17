7ea4507579a772da74056c68cbffb11fd9efd985
Sulav <sulavshrestha023@gmail.com>
2022-06-17 12:04:22 +0545

    stater

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React from 'react'
+import { useGlobalContext } from './context'
 
-export default function App() {
-  return <div>App</div>;
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+// items
+
+function App() {
+  // if (loading) {
+  //   return (
+  //     <div className='loading'>
+  //       <h1>Loading...</h1>
+  //     </div>
+  //   )
+  // }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AppProvider } from './context';
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
